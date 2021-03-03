0542a850814c9d3e078d77e131decfb592a22ff2
Thomas Weideman <thomas.weideman@gmail.com>
2021-03-03 00:53:28 +0000

    Add fourth round of queries

---
+++ 4_queries/which_teachers_assisted_and_how_many_ars.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY teacher, cohort
+HAVING cohorts.name = 'JUL02'
+ORDER BY teacher;
