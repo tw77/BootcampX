e74d14c7b48c2b44bdf51091d2a91b954ca4d82a
Thomas Weideman <thomas.weideman@gmail.com>
2021-03-04 03:38:37 +0000

    Connect and query using node-postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY teacher, cohort
+HAVING cohorts.name LIKE '%${process.argv[2]}%'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
